dc081cb01dcf55ea388a5c24e89b16fc81e3e336
lazerpy <mob4rre@gmail.com>
2025-06-23 20:13:33 +0300

    Update Collection.jsx
    For vercel error

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title'
-import ProductsItem from '../components/ProductsITem'
+import ProductsItem from '../components/ProductsItem'
 import { assets } from '../assets/assets'
 
 
